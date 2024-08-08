67a19d34ea6fcd35aafa5c9573d5344e8d0cd78d
rtcs-org <133113132+rtcs-org@users.noreply.github.com>
2024-08-08 11:01:21 +0300

    only enable ASR

---
+++ Enable-ASR.ps1
@@ -0,0 +1,57 @@
+# Require elevation for script run
+Write-Output "Elevating privileges for this process`n"
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"")
+    exit
+}
+
+Write-Host "Enabling Windows Defender Attack Surface Reduction Rules`n"
+
+#https://learn.microsoft.com/en-us/defender-endpoint/defender-endpoint-demonstration-attack-surface-reduction-rules
+
+Write-Host "Block abuse of exploited vulnerable signed drivers`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 56a863a9-875e-4185-98a7-b882c64b5ce5 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block Adobe Reader from creating child processes`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block all Office applications from creating child processes`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids d4f940ab-401b-4efc-aadc-ad5f3c50688a -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block credential stealing from the Windows local security authority subsystem (lsass.exe)`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block executable content from email client and webmail`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids be9ba2d9-53ea-4cdc-84e5-9b1eeee46550 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block execution of potentially obfuscated scripts`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 5beb7efe-fd9a-4556-801d-275e5ffc04cc -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block JavaScript or VBScript from launching downloaded executable content`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids d3e037e1-3eb8-44c8-a917-57927947596d -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block Office applications from creating executable content`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 3b576869-a4ec-4529-8536-b80a7769e899 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block Office applications from injecting code into other processes`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 75668c1f-73b5-4cf0-bb93-3ecf5cb7cc84 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block Office communication application from creating child processes`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 26190899-1602-49e8-8b27-eb1d0a1ce869 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block untrusted and unsigned processes that run from USB`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block use of copied or impersonated system tools`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids c0033c00-d16d-4114-a5a0-dc9b3a7d2ceb -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Block Win32 API calls from Office macros`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids 92e97fa1-2edf-4476-bdd6-9dd0b4dddc7b -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "Use advanced protection against ransomware`n"
+Add-MpPreference -AttackSurfaceReductionRules_Ids c1db55ab-c21a-4637-bb3f-a12568109d35 -AttackSurfaceReductionRules_Actions Enabled
+
+Write-Host "ASR update done!"  -ForegroundColor Green
+
+Write-Host -NoNewLine 'Press any key to exit...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
